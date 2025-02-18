05d3f58db6eb265e32d94bbf7b00fbc844390565
KasperPersson <kape0010@stud.kea.dk>
2025-02-18 10:23:41 +0100

    first commit

---
+++ src/main/java/org/example/pullrequestexercise1/PullRequestExercise1Application.java
@@ -0,0 +1,13 @@
+package org.example.pullrequestexercise1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PullRequestExercise1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PullRequestExercise1Application.class, args);
+    }
+
+}
